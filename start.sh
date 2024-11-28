01f65ca048ec0a1488dfcdc91430994ac29241ff
israelmanzi <manziisrael99@gmail.com>
2024-11-28 18:31:30 +0200

    [fx]: minor fixes

--- start.sh
+++ start.sh
@@ -6,4 +6,8 @@ if [ ! -f ./otel-agent.jar ]; then
   wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v2.10.0/opentelemetry-javaagent.jar -O ./otel-agent.jar
 fi
 
+if [ ! -d ./target ] || [ ! -f ./target/spring-boot-docker-postgres.jar ]; then
+  mvn clean package
+fi
+
 docker compose up --build
